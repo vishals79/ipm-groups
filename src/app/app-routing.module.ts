51d38b6009b5c234919e6888aee075238aecfa4a
vishals <vishals@PRINHYLTPAP1333.local>
2019-01-25 01:50:14 +0530

    Commented route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 
 const route: Routes = [
   { path: '', loadChildren: '../app/home-page/home-page.module#HomePageModule'},
-  { path: 'view-results', loadChildren: '../app/view-results/view-results.module#ViewResultsModule'}
+  // { path: 'view-results', loadChildren: '../app/view-results/view-results.module#ViewResultsModule'}
 ];
 
 @NgModule({
